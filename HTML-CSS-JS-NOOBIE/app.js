d0b51d94e7af1387db8d6a3bedbd13ac1535c7ff
revelation136 <83710384+revelation136@users.noreply.github.com>
2021-05-05 21:25:42 +0800

    Add files via upload

---
+++ HTML-CSS-JS-NOOBIE/app.js
@@ -0,0 +1,15 @@
+// defining Functions
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo')
+
+// display mobile menu
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Show Active Menu when Scrolling
